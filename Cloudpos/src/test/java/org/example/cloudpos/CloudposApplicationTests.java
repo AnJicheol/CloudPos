d875c81eb8bb7f5330194a3cce7dfe48f908c196
AnJicheol <kany0511@gmail.com>
2025-10-30 13:10:51 +0900

    init

---
+++ Cloudpos/src/test/java/org/example/cloudpos/CloudposApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
