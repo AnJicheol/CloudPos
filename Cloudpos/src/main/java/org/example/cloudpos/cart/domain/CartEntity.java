f8fd34ac6e8c80b2ca78c62aed3bb78444d73ca9
k1myj <kimyujeong1204@gmail.com>
2025-11-03 23:25:29 +0900

    feat: cartEntity 구현

--- Cloudpos/src/main/java/org/example/cloudpos/cart/domain/CartEntity.java
+++ Cloudpos/src/main/java/org/example/cloudpos/cart/domain/CartEntity.java
@@ -1,5 +1,26 @@
 package org.example.cloudpos.cart.domain;
 
-public class CartEntity {
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="carts")
+@Getter
+@NoArgsConstructor
+public class CartEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="cart_id", nullable = false, unique = true)
+    private Long cartId;
+
+    @Column(name="user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="state", nullable=false)
+    private CartState state= CartState.EMPTY;
 }
