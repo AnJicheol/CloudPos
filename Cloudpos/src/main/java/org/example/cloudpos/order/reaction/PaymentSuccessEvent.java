4a6082b71001f24573b9439c1d9a8ede0264db56
AnJicheol <kany0511@gmail.com>
2025-11-07 11:22:40 +0900

    feat: 도메인 및, 결제 상태 인터페이스

---
+++ Cloudpos/src/main/java/org/example/cloudpos/order/reaction/PaymentSuccessEvent.java
@@ -0,0 +1,4 @@
+package org.example.cloudpos.order.reaction;
+
+
+public record PaymentSuccessEvent(String orderId) {}
--- Cloudpos/src/main/java/org/example/cloudpos/order/service/OrderServiceImpl.java
+++ Cloudpos/src/main/java/org/example/cloudpos/order/service/OrderServiceImpl.java
@@ -1,11 +1,28 @@
 package org.example.cloudpos.order.service;
 
 
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    private final ApplicationEventPublisher publisher;
 
 
+    @Override
+    public void onPaymentSuccess(String orderId) {
+
+    }
+
+    @Override
+    public void onPaymentFailure(String orderId) {
+
+    }
+
+    @Override
+    public void onPaymentCanceled(String orderId) {
+
+    }
 }
