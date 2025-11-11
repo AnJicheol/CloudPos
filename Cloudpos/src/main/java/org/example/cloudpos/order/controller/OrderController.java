9fe9024fa7d4322654aed5e6233fab965698f501
AnJicheol <kany0511@gmail.com>
2025-11-11 14:08:38 +0900

    feat: oder api 문서 작성

--- Cloudpos/src/main/java/org/example/cloudpos/order/controller/OrderController.java
+++ Cloudpos/src/main/java/org/example/cloudpos/order/controller/OrderController.java
@@ -1,5 +1,9 @@
 package org.example.cloudpos.order.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.cloudpos.order.domain.Order;
 import org.example.cloudpos.order.service.OrderService;
@@ -16,6 +20,19 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(
+            summary = "결제 시작",
+            description = "장바구니 ID를 받아 장바구니 상태를 '결제 진행'으로 변경하고, " +
+                    "현재 장바구니 명세와 할인 정보를 기준으로 최종 결제 금액을 확정합니다. " +
+                    "한 번 확정된 금액은 이후에 변경되지 않습니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "주문 생성/결제 시작 성공",
+            content = @Content(
+                    schema = @Schema(implementation = Order.class)
+            )
+    )
     @PostMapping("/start-payment/{cartId}")
     public ResponseEntity<Order> startPayment(@PathVariable String cartId) {
         Order order = orderService.startPayment(cartId);
