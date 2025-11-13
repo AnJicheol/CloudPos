cf16c5805101c53eddf1147ea3fb4853b5735a10
k1myj <kimyujeong1204@gmail.com>
2025-11-13 17:46:24 +0900

    refactor: cart api, lietener

--- Cloudpos/src/main/java/org/example/cloudpos/cart/dto/ProductSummary.java
+++ Cloudpos/src/main/java/org/example/cloudpos/cart/dto/ProductSummary.java
@@ -1,14 +1,6 @@
 package org.example.cloudpos.cart.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductSummary {
-    private String productId;
-    private String name;
-    private int price;
-}
+
+public record ProductSummary(String productId, String name, long price) {}
+
